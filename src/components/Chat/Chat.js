3f9603077490c6267ae3e81f9ff0b62c7acfdae2
Solomon Mekuria <70961231+smekuria1@users.noreply.github.com>
2023-06-16 12:13:21 -0400

    moved server to gcp

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-import './Chat.css';
-import { useLocation } from 'react-router';
-import InfoBar from '../InfoBar/InfoBar.js';
-import Input from '../Input/Input.js';
-import Messages from '../Messages/Messages.js';
-import TextContainer from '../TextContainer/TextContainer.js';
+import "./Chat.css";
+import { useLocation } from "react-router";
+import InfoBar from "../InfoBar/InfoBar.js";
+import Input from "../Input/Input.js";
+import Messages from "../Messages/Messages.js";
+import TextContainer from "../TextContainer/TextContainer.js";
 let socket;
 const Chat = () => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://smek-chat.herokuapp.com/';
+  const ENDPOINT = "https://s-chat.solomonmek.website/";
   const location = useLocation();
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -25,22 +25,22 @@ const Chat = () => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room }, (error) => {
+    socket.emit("join", { name, room }, (error) => {
       if (error) {
         alert(error);
       }
     });
     return () => {
-      socket.emit('disconnect');
+      socket.emit("disconnect");
       socket.off();
     };
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
-    socket.on('message', (message) => {
+    socket.on("message", (message) => {
       setMessages([...messages, message]);
     });
-    socket.on('roomData', ({ users }) => {
+    socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
   }, [messages, users]);
@@ -48,7 +48,7 @@ const Chat = () => {
   const sendMessage = (event) => {
     event.preventDefault();
     if (message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+      socket.emit("sendMessage", message, () => setMessage(""));
     }
   };
 
